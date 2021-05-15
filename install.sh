06fed1ac0c40a8f8f9b4759621ab78f4509c642c
manote101 <manote.usa@gmail.com>
2021-05-15 14:16:54 +0000

    ok

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 # run command to install
-sudo dpkg –i *.deb
+sudo dpkg –i containerd.io_1.4.4-1_amd64.deb \
+docker-ce-cli_20.10.5~3-0~ubuntu-focal_amd64.deb \
+docker-ce_20.10.5~3-0~ubuntu-focal_amd64.deb
