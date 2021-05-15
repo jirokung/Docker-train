87f6b9dcdebc6e072ea6cb385bc1f96a5e2e8f5a
manote101 <manote.usa@gmail.com>
2021-05-15 05:09:21 +0000

    add again

---
+++ get-installaer.sh
@@ -0,0 +1,8 @@
+# Follow instructions in https://docs.docker.com/engine/install/ubuntu/ 
+# then go to https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/
+
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/containerd.io_1.4.4-1_amd64.deb 
+
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce-cli_20.10.5~3-0~ubuntu-focal_amd64.deb
+
+wget https://download.docker.com/linux/ubuntu/dists/focal/pool/stable/amd64/docker-ce_20.10.5~3-0~ubuntu-focal_amd64.deb
